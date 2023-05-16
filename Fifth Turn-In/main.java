fa7cf8f166f8cd807a27e6cc0f7e25e887d5349f
Kristian <xristosloukas2001@gmail.com>
2023-05-16 21:29:08 +0300

    just a change in the message

--- Fifth Turn-In/main.java
+++ Fifth Turn-In/main.java
@@ -2,7 +2,7 @@
 
 public class main {
     public static void main(String[] args){
-        System.out.println("Hello World !!!");
+        System.out.println("Hello World  ( Texnologia Logismikoy ) !!!");
 
     }
 }
