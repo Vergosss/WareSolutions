0b4480c12cb7005f20e875ae47005e3fdae69e37
Vergosss <72676680+Vergosss@users.noreply.github.com>
2023-05-16 21:38:28 +0300

    Added Vergos commit message and a comment.

--- Fifth Turn-In/main.java
+++ Fifth Turn-In/main.java
@@ -1,8 +1,10 @@
 // example
 
 public class main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello World  ( Texnologia Logismikoy ) !!!");
+        System.out.println("Vergos commit");
+        //
 
     }
 }
