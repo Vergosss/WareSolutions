03ced02387e59a5d91b8fc4fe0b54413b8014e0b
Kristian <xristosloukas2001@gmail.com>
2023-05-19 21:10:39 +0300

    Class Profile (Contains: getters and setters)

---
+++ Fifth Turn-In/Profile.java
@@ -0,0 +1,37 @@
+public class Profile {
+    private String customerName;
+    private String password;
+    private String location;
+
+    // Constructor
+    public Profile(String customerName, String password, String location) {
+        this.customerName = customerName;
+        this.password = password;
+        this.location = location;
+    }
+
+    // Getters and Setters For EditProfile Use Case
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
