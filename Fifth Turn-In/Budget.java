2dc98ac7c92b84ca58d6d03e591a9ee2fc752106
Kristian <xristosloukas2001@gmail.com>
2023-05-19 23:47:37 +0300

    New Class -> Budget

---
+++ Fifth Turn-In/Budget.java
@@ -0,0 +1,16 @@
+public class Budget {
+    private float income;
+    private float expenses;
+    private boolean viability;
+
+
+// Constructor
+
+public Budget(float income, float expenses, boolean viability){
+    this.income = income;    
+    this.expenses = expenses;
+    this.viability = viability;
+}
+
+
+}
