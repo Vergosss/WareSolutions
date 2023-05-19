73a493c9abf8d9acdf2e52f94c29477c702f3a3c
Kristian <xristosloukas2001@gmail.com>
2023-05-20 00:12:24 +0300

    added wh assosiation

--- Fifth Turn-In/Budget.java
+++ Fifth Turn-In/Budget.java
@@ -2,14 +2,15 @@ public class Budget {
     private float income;
     private float expenses;
     private boolean viability;
-
+    private Warehouse warehouse;
 
 // Constructor
 
-public Budget(float income, float expenses, boolean viability){
+public Budget(float income, float expenses, boolean viability, Warehouse warehouse){
     this.income = income;    
     this.expenses = expenses;
     this.viability = viability;
+    this.warehouse = warehouse;
 }
 
 //The beginning of getters / setters:
