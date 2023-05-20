b7ac23b8086a282302d4d16d80ae629a3656fd41
Vergosss <{72676680}+{Vergosss}@users.noreply.github.com>
2023-05-20 19:38:25 +0300

    Updated attributes , constructor ,setters and getters for the StorageLocation class.

--- Fifth Turn-In/StorageLocation.java
+++ Fifth Turn-In/StorageLocation.java
@@ -1,3 +1,23 @@
 public class StorageLocation {
-    
+    private int aisle;
+    private int shelf;
+    public StorageLocation(int aisle, int shelf) {
+        this.aisle = aisle;
+        this.shelf = shelf;
+
+    }
+    public int getAisle(){
+        return this.aisle;
+    }
+    public int getShelf(){
+        return this.shelf;
+
+    }
+    public void setAisle(int aisle){
+        this.aisle = aisle;
+    }
+    public void setShelf(int shelf){
+        this.shelf = shelf;
+
+    }
 }
