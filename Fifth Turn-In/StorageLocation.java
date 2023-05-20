6c7781f26402568cf5ee9d527a3a378307f608b8
Kristian <xristosloukas2001@gmail.com>
2023-05-20 20:00:40 +0300

    added assosiation between warehouse

--- Fifth Turn-In/StorageLocation.java
+++ Fifth Turn-In/StorageLocation.java
@@ -1,21 +1,38 @@
 public class StorageLocation {
+    private String warehouse;
     private int aisle;
     private int shelf;
-    public StorageLocation(int aisle, int shelf) {
+
+    // Contructor 
+    public StorageLocation(String warehouse, int aisle, int shelf) {
+        this.warehouse = warehouse;
         this.aisle = aisle;
         this.shelf = shelf;
 
     }
+
+    // Getters and Setters
     public int getAisle(){
         return this.aisle;
     }
+
     public int getShelf(){
         return this.shelf;
 
     }
+
+    public String getWarehouse() {
+        return warehouse;
+    }
+    
+    public void setWarehouse(String warehouse) {
+        this.warehouse = warehouse;
+    }
+
     public void setAisle(int aisle){
         this.aisle = aisle;
     }
+
     public void setShelf(int shelf){
         this.shelf = shelf;
 
