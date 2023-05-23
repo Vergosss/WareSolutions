90ce30345ea2ab54a504e3ca3127f9acf2b7651c
Kristian <xristosloukas2001@gmail.com>
2023-05-23 14:04:20 +0300

    G/S for Product class

--- Fifth Turn-In/Product.java
+++ Fifth Turn-In/Product.java
@@ -4,4 +4,45 @@ public class Product {
     private String description;
     private String category;
 
+// Constructor
+public Product(String identifier, String name, String description, String category) {
+    this.identifier = identifier;
+    this.name = name;
+    this.description = description;
+    this.category = category;
+}
+
+// Getters and Setters
+public String getIdentifier() {
+    return identifier;
+}
+
+public void setIdentifier(String identifier) {
+    this.identifier = identifier;
+}
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public String getDescription() {
+    return description;
+}
+
+public void setDescription(String description) {
+    this.description = description;
+}
+
+public String getCategory() {
+    return category;
+}
+
+public void setCategory(String category) {
+    this.category = category;
+}
+
 }
