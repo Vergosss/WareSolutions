c31fba7f9c9adb890724df7c518e93b84c64e2bf
stvlachos <stavrosv30@gmail.com>
2023-05-25 17:50:22 +0300

    Added the class attributes of the Card Class.

--- Fifth Turn-In/Card.java
+++ Fifth Turn-In/Card.java
@@ -1,3 +1,14 @@
+import java.time.LocalDate;
 public class Card{
+
+    // Class attributes
+    private String name;
     
+    private int year;
+    private int month;
+    private int day;
+    LocalDate expiration_date = LocalDate.of(year,month,day);
+
+
+
 }
