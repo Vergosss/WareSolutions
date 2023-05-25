086a3f01fb3b9e31a2bdc4bece9ed249114a367c
Vergosss <72676680+Vergosss@users.noreply.github.com>
2023-05-25 20:19:21 +0300

    Changed main to Main class including code to display a simple login form in JavaFX. Issue: logo.

--- Fifth Turn-In/main.java
+++ Fifth Turn-In/main.java
@@ -1,10 +1,28 @@
 // example
 
-public class main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+public class Main extends Application {
+
+    private static Stage stg;
+    public void start(Stage primaryStage) throws Exception{
+        stg = primaryStage;
+        primaryStage.setResizable(true);
+        Parent root = FXMLLoader.load(getClass().getResource("LoginForm.fxml"));
+        primaryStage.setTitle("Falcon");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+    public void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stg.getScene().setRoot(pane);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World  ( Texnologia Logismikoy ) !!!");
-        System.out.println("Vergos commit");
-        // Commit and push
+        launch(args);
 
     }
 }
