de9e8b991991d1e2c62b931b29488b241d9e6eea
Efstathios Ravanos <stathisrav@gmail.com>
2023-05-26 21:13:17 +0300

    added Constructor

--- Fifth Turn-In/ShoppingCart.java
+++ Fifth Turn-In/ShoppingCart.java
@@ -2,4 +2,11 @@ public class ShoppingCart {
     // Class attributes
     private String products;
     
+
+     // Constructor
+
+     public ShoppingCart(String products) {
+        this.products = products;
+    }
+    
 }
