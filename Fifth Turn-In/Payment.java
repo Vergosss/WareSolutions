afa21fea3c1868a25c4b3638ab3e02c72ca896fe
Vergosss <72676680+Vergosss@users.noreply.github.com>
2023-05-27 23:39:45 +0300

    Added new attribute: amount. Added its getters and setters.

--- Fifth Turn-In/Payment.java
+++ Fifth Turn-In/Payment.java
@@ -5,13 +5,15 @@ public class Payment {
     private String identifier;
     private LocalDate date;
     private String method;
+    private float amount;
 
     // Constructor
 
-    public Payment(String identifier, String method) {
+    public Payment(String identifier, String method, float amount) {
         this.identifier = identifier;
         this.date = LocalDate.now();
         this.method = method;
+        this.amount = amount;
 
     }
 
@@ -25,7 +27,7 @@ public class Payment {
         this.identifier = identifier;
     }
 
-    // getters and setters for payment_method
+    // getters and setters for method
 
     public String getMethod() {
         return this.method;
@@ -33,5 +35,14 @@ public class Payment {
 
     public void setMethod(String method) {
         this.method = method;
+    }
+
+    // getters and setters for amount
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public float getAmount() {
+        return this.amount;
     }
 }
