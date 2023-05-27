3ab10c3ccea5e21ee7ad94a2b9004593af9544ac
stvlachos <stavrosv30@gmail.com>
2023-05-27 20:24:22 +0300

    Added the constructor on the Payment Class

--- Fifth Turn-In/Payment.java
+++ Fifth Turn-In/Payment.java
@@ -3,7 +3,17 @@ import java.time.LocalDate;
 //Class attributesb
 public class Payment {
     private String identifier;
-    private LocalDate payment_Date= LocalDate.now();
+    private LocalDate payment_date;
     private String method;
+
+    // Constructor
+
+    public Payment(String identifier, String method) {
+        this.identifier = identifier;
+        this.payment_date = LocalDate.now();
+        this.method = method;
+        
+    }
+
 
 }
