d18ed83ad4c84d63b5da21f4ae47e5a4970eae7e
stvlachos <stavrosv30@gmail.com>
2023-05-27 19:55:47 +0300

    Added attributes of Payment Class

--- Fifth Turn-In/Payment.java
+++ Fifth Turn-In/Payment.java
@@ -1,3 +1,9 @@
+import java.time.LocalDate;
+
+//Class attributesb
 public class Payment {
-    
+    private String identifier;
+    private LocalDate payment_Date= LocalDate.now();
+    private String method;
+
 }
