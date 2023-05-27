d8b7095091756c8b09cd5a013100a48e6755154f
Efstathios Ravanos <stathisrav@gmail.com>
2023-05-27 17:24:34 +0300

    added getters and setters in class Date

--- Fifth Turn-In/Date.java
+++ Fifth Turn-In/Date.java
@@ -15,4 +15,35 @@ public class Date {
     
 }
 
+ // The beginning of getters / setters:
+ public int getday() {
+    return this.day;
+}
+
+public void setday(int day) {
+    this.day = day;
+}
+
+
+public int getmonth() {
+    return this.month;
+}
+
+public void setmonth(int month) {
+    this.month = month;
+}
+
+
+public int getyear() {
+    return this.year;
+}
+
+public void setyear(int year) {
+    this.year = year;
+}
+
+
+// end of getters and setters.
+//
+
 }
