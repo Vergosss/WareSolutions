4701718184f981d8f8e3f52327effb72bc6b2dfc
Efstathios Ravanos <stathisrav@gmail.com>
2023-05-27 17:10:30 +0300

    added getters and setters in class Order

--- Fifth Turn-In/Order.java
+++ Fifth Turn-In/Order.java
@@ -18,4 +18,43 @@ public class Order {
 
     }
 
+    // The beginning of getters / setters 
+    public Date getDate() {
+        return this.Date;
+    }
+
+    public void setDate(Date Date) {
+        this.Date = Date;
+    }
+
+
+    public String getidentifier() {
+        return this.identifier;
+    }
+
+    public void setidentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+
+    public String getcustomer() {
+        return this.customer;
+    }
+
+    public void setcustomer(String customer) {
+        this.customer = customer;
+    }
+
+
+    public float getpayment() {
+        return this.payment;
+    }
+
+    public void setpayment(float payment) {
+        this.payment = payment;
+    }
+
+     // end of getters and setters.
+    //
+
 }
