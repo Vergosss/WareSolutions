7afad0f57534da560575dd842c962d2450b1cdbf
stvlachos <stavrosv30@gmail.com>
2023-05-28 15:43:13 +0300

    Added the Constructor of Supplier Class

--- Fifth Turn-In/Supplier.java
+++ Fifth Turn-In/Supplier.java
@@ -1,4 +1,14 @@
 public class Supplier {
     private String identifier;
     private String brand;
+
+    //Constructor 
+
+    public Supplier(String identifier, String brand) {
+        this.identifier = identifier;
+        this.brand = brand;
+     }
+
+     
+
 }
