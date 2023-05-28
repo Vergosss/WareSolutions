2755a7d641eaaaefa1c1461389a62d9abe07776e
stvlachos <stavrosv30@gmail.com>
2023-05-28 19:53:32 +0300

    Added ContactSupplier Class constructor

--- Fifth Turn-In/ContactSupplier.java
+++ Fifth Turn-In/ContactSupplier.java
@@ -2,4 +2,13 @@ public class ContactSupplier {
     private Date date;
     private String supplier;
     private String topic;
+
+    // Constructor 
+
+    public ContactSupplier(Date date,String supplier, String topic){
+        this.date=date;
+        this.supplier=supplier;
+        this.topic=topic;
+    }
+
 }
