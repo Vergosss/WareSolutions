1275b40c44a7acbb6392a66028132003b4c8f4bd
Vergosss <72676680+Vergosss@users.noreply.github.com>
2023-05-29 22:38:21 +0300

    Declared checkDateValidity() and deleteDates() methods according to our Class Diagram.

--- Fifth Turn-In/Date.java
+++ Fifth Turn-In/Date.java
@@ -1,49 +1,54 @@
 public class Date {
-    
+
     // Class attributes
     private int day;
     private int month;
     private int year;
 
-
     // Constructor
 
- public Date(int day, int month, int year) {
-    this.day = day;
-    this.month = month;
-    this.year = year;
-    
-}
+    public Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
 
- // The beginning of getters / setters:
- public int getDay() {
-    return this.day;
-}
+    }
 
-public void setDay(int day) {
-    this.day = day;
-}
+    // The beginning of getters / setters:
+    public int getDay() {
+        return this.day;
+    }
 
+    public void setDay(int day) {
+        this.day = day;
+    }
 
-public int getMonth() {
-    return this.month;
-}
+    public int getMonth() {
+        return this.month;
+    }
 
-public void setMonth(int month) {
-    this.month = month;
-}
+    public void setMonth(int month) {
+        this.month = month;
+    }
 
+    public int getYear() {
+        return this.year;
+    }
 
-public int getYear() {
-    return this.year;
-}
+    public void setYear(int year) {
+        this.year = year;
+    }
 
-public void setYear(int year) {
-    this.year = year;
-}
+    // Sequence diagram 13 and Use Case 13
+    public void checkDateValidity() {
 
+    }
 
-// end of getters and setters.
-//
+    // Sequence diagram 13 and Use Case 13
+    public void deleteDates() {
+
+    }
+    // end of getters and setters.
+    //
 
 }
