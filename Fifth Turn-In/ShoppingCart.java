ad05ce86654869fe953e2475346c58b7e6b87235
Kristian <xristosloukas2001@gmail.com>
2023-06-01 14:52:59 +0300

    Association: ShoppingCart-Order

--- Fifth Turn-In/ShoppingCart.java
+++ Fifth Turn-In/ShoppingCart.java
@@ -4,30 +4,37 @@ import java.util.ArrayList;
 public class ShoppingCart {
     // Class attributes
     private List<Product> products = new ArrayList<>();
-
+    private Order order; //insance variable order of type Order (association)
+   
     // Constructor
-
-    public ShoppingCart(List<Product> products) {
+    public ShoppingCart(List<Product> products, Order order) {
         this.products = products;
+        this.order = order;
     }
 
-
-     // The beginning of getters / setters 
-     public List<Product> getProducts() {
+     // The beginning of getters / setters:
+    public List<Product> getProducts() {
         return this.products;
     }
 
     public void setProducts(List<Product> products) {
         this.products = products;
     }
+    
+    public Order getOrder() {
+        return order;
+    }
 
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 
      // end of getters and setters.
     //
 
     // method to create a new shopping cart.
     public static ShoppingCart createCart() {
-        return new ShoppingCart(new ArrayList<>());
+        return new ShoppingCart(new ArrayList<>(), null);
     }
 
 //in above code block, the method createCart() returns an instance of the ShoppingCart.
