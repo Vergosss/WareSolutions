53c3af6afc6c817242524dc8549ca94f976dbed3
Vergosss <72676680+Vergosss@users.noreply.github.com>
2023-06-05 14:04:25 +0300

    Added class Product methods regarding Product's editing of value, information etc.

--- Fifth Turn-In/Product.java
+++ Fifth Turn-In/Product.java
@@ -1,53 +1,72 @@
 public class Product {
-    //Class attributes
+    // Class attributes
     private String identifier;
     private String name;
     private String description;
     private String category;
 
-// Constructor
-public Product(String identifier, String name, String description, String category) {
-    this.identifier = identifier;
-    this.name = name;
-    this.description = description;
-    this.category = category;
-}
+    // Constructor
+    public Product(String identifier, String name, String description, String category) {
+        this.identifier = identifier;
+        this.name = name;
+        this.description = description;
+        this.category = category;
+    }
 
-// Getters and Setters
-public String getIdentifier() {
-    return this.identifier;
-}
+    // Getters and Setters
+    public String getIdentifier() {
+        return this.identifier;
+    }
 
-public void setIdentifier(String identifier) {
-    this.identifier = identifier;
-}
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
 
-public String getName() {
-    return this.name;
-}
+    public String getName() {
+        return this.name;
+    }
 
-public void setName(String name) {
-    this.name = name;
-}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-public String getDescription() {
-    return this.description;
-}
+    public String getDescription() {
+        return this.description;
+    }
 
-public void setDescription(String description) {
-    this.description = description;
-}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-public String getCategory() {
-    return this.category;
-}
+    public String getCategory() {
+        return this.category;
+    }
 
-public void setCategory(String category) {
-    this.category = category;
-}
+    public void setCategory(String category) {
+        this.category = category;
+    }
 
-// method: productExists(quantity: int)
-// quantity will exist in our database 
+    // method: productExists(quantity: int)
+    // quantity will exist in our database
+    public boolean productExists(int quantity) {
+        return true;// dummy
 
+    }
+
+    public void updateProductInfo() {
+
+    }
+
+    public void addProductToStorage() {
+
+    }
+
+    public void assignValueToProduct(float value) {
+
+    }
+
+    public void checkProductQuantityAvailiability() {
+
+    }
 
 }
